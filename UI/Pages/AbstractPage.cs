10b285078d821a7b24e734c038726accd4881076
Davit Simonyan <davitsimonyan74@gamil.com>
2023-02-06 22:46:38 +0400

    Refactor Abstract class

--- UI/Pages/AbstractPage.cs
+++ UI/Pages/AbstractPage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V107.Page;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace UI
 {
-    internal class AbstractPage
+    internal abstract class AbstractPage
     {
+      protected IWebDriver driver;
+        public AbstractPage(IWebDriver driver) {
+            this.driver = driver;
+        }
+
+        public abstract void Navigate();
+
+        public virtual bool isLoaded()
+        {
+            return driver.Url.Contains("stackoverflow.commmmimi");
+
+        }
+
+    
+
+
     }
 }
