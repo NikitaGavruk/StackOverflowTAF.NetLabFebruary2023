af57713f82850c976d4f203cc4c0f8d5aa8eff85
Payam Dehkordy <payam.dehkordy@gmail.com>
2023-02-09 11:44:15 +0400

    update webDeriverFactory

--- Core/WebDriver/WebDriverFactory.cs
+++ Core/WebDriver/WebDriverFactory.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,25 @@ namespace AutomationTeamProject.WebDriver
             Firefox
         }
 
-        public static IWebDriver 
+        public static IWebDriver GetDriver(Browsertype browser, int TimeoutSec)
+        {
+            IWebDriver WebDriver = null;
+            switch (browser)
+            {
+                case Browsertype.chrome:
+                    {
+                        var Service = ChromeDriverService.CreateDefaultService();
+                        var Option = new ChromeOptions();
+                        WebDriver = new ChromeDriver(Service, Option, TimeSpan.FromSeconds(TimeoutSec));
+                        break;
+                    }
+                case Browsertype.Firefox:
+                    {
+                        WebDriver = new FirefoxDriver();
+                        break;
+                    }
+            }
+            return WebDriver;
+        }
     }
 }
