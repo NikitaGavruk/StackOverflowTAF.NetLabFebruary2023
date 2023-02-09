bc2f8883f2e38aeb4083c128fac475f51d3a9fa8
Payam Dehkordy <payam.dehkordy@gmail.com>
2023-02-09 11:32:09 +0400

    Add WebriverFactory class
    Add WebDriverFactory class in WebDriver Folder

---
+++ Core/WebDriver/WebDriverFactory.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTeamProject.WebDriver
+{
+    internal class WebDriverFactory
+    {
+        public enum Browsertype
+        {
+            chrome,
+            Firefox
+        }
+
+        public static IWebDriver 
+    }
+}
