765baa73602308223d4b3739a9aa8fd5d0510914
Payam Dehkordy <payam.dehkordy@gmail.com>
2023-02-15 11:03:02 +0400

    add webDriver again

---
+++ UI/WebDriver/Browser.cs
@@ -0,0 +1,64 @@
+﻿using OpenQA.Selenium;
+using System;
+using static AutomationTeamProject.WebDriver.WebDriverFactory;
+
+namespace AutomationTeamProject.WebDriver
+{
+    public class Browser
+    {
+        public static Browsertype _currentBrowser;
+        private static Browser _currentInstance;
+        private static string _browser;
+        private static int ImplWait;
+        private static IWebDriver webDriver;
+
+        // pars the parameter that ar taken from App.config
+        private static void InitParams()
+        {
+            ImplWait = Convert.ToInt32(Configuration.ElementTimeout);
+            _browser = Configuration.Browser;
+            Enum.TryParse(_browser, out _currentBrowser);
+        }
+
+        private Browser()
+        {
+            InitParams();
+            webDriver = WebDriverFactory.GetDriver(_currentBrowser, ImplWait);
+        }
+
+        public static Browser Instance => _currentInstance ?? (_currentInstance = new Browser());
+        
+        //Get the Driver
+        public static IWebDriver GetDriver()
+        {
+            return webDriver;
+        }
+        
+        //Windows maximaizer
+        public static void WindowMaximaze()
+        {
+            webDriver.Manage().Window.Maximize();
+        }
+
+        //Navigator
+        public static void NavigateTo(string url)
+        {
+            webDriver.Navigate().GoToUrl(url);
+        }  
+        
+        //Navigator
+        public static void StartNavigate()
+        {
+            webDriver.Navigate().GoToUrl(Configuration.StartUrl);
+        }
+
+        //Quit the browser
+        public static void QuiteBrowser()
+        {
+            webDriver.Quit();
+            _currentInstance = null;
+            webDriver = null;
+            _browser = null;
+        }
+    }
+}
---
+++ UI/WebDriver/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace AutomationTeamProject.WebDriver
+{
+    public class Configuration
+    {
+
+        //app.config data exrtractor method
+        public static string GetEnvironmentVar(string var, string defaultaVar)
+        {
+            //string SourceCodeConfigeFile = $"{Assembly.GetExecutingAssembly().Location}.config";
+            //string OutputonfigFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).FilePath;
+            //File.Copy(SourceCodeConfigeFile, OutputonfigFile, true);
+            //return ConfigurationManager.AppSettings[var] ??
+	      return defaultaVar;
+        }
+
+        //app.config Timeout extraction
+        public static string ElementTimeout => GetEnvironmentVar("ElementTimeout", "30");
+
+        //app.config Browser extraction
+        public static string Browser => GetEnvironmentVar("Browser", "Chrome");
+
+        //app.config starting Url  extraction
+        public static string StartUrl => GetEnvironmentVar("StartUrl", "https://stackoverflow.com");
+
+    }
+}
