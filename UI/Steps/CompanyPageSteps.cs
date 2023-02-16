580505039c25e1b325a7ee5605fb269fee7853e8
Erik Movsesyan <erik_movsesyan@epam.com>
2023-02-16 19:21:50 +0400

    Fixing a small path-related issue in UI/Steps/CompanyPageSteps.cs

--- UI/Steps/CompanyPageSteps.cs
+++ UI/Steps/CompanyPageSteps.cs
@@ -4,7 +4,7 @@ using UI.Pages;
 namespace UI.Steps {
     internal class CompanyPageSteps {
 
-        private static readonly string tagToSearch = new XML_Reader("C:/Users/User/OneDrive/Desktop/.NET Lab/.NET Project/.Net-Lab/UI/Tests/TestData.xml").GetTextFromNode("//TagToSearch");
+        private static readonly string tagToSearch = new XML_Reader("UI/Tests/TestData.xml").GetTextFromNode("//TagToSearch");
 
         CompaniesPage companyPage = new CompaniesPage();
 
