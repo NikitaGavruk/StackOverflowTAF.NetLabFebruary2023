0e259f47f1d7036437a9fe9d29384c63ee59c109
Laura Hambardzumyan <90081975+LauraHambardzumyan@users.noreply.github.com>
2023-02-20 20:03:15 +0400

    Create APIUtils.cs

---
+++ API/RestUtils/APIUtils.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.RestUtils
+{
+    internal class APIUtils
+    {
+        public static HttpWebResponse MakeRequest(HttpWebRequest request)
+        {
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            return response;
+        }
+        public static string GetBody(HttpWebResponse response)
+        {
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string strResponse = reader.ReadToEnd();
+            return strResponse;
+        }
+
+        public static List<T> DeserializeRootObject(string body) => JsonConvert.DeserializeObject<List<T>>(body);
+        //Switch T with the part we need
+
+    }
+}
