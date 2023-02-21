a1eaa536e8ce98b5d99e93e2775b0a97d6501e50
Payam Dehkordy <payam.dehkordy@gmail.com>
2023-02-21 15:10:40 +0400

    Add Client class to API project

---
+++ API/APIUtils/Client.cs
@@ -0,0 +1,41 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.APIUtils
+{
+    internal class Client
+    {
+        private static string URL = "https://stackoverflow.com";
+        private static string Username = "";
+        private static string Password = "";
+        private Client _currentClient;
+        private RestClient _Client;
+
+        private Client()
+        {
+            _Client = new RestClient(URL);
+        }
+
+        public Client Instance => _currentClient ?? (_currentClient = new Client());
+
+        public RestClient GetClient => _Client;
+
+        public RestClient GetClientAuth()
+        {
+            _Client.Authenticator = new HttpBasicAuthenticator(Username,Password);
+            return _Client;
+        }
+
+        public void QuitClient()
+        {
+            _currentClient = null;
+            _Client = null;
+        }
+    }
+
+}
