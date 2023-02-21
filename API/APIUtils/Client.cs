779c5da8faf5065bfe0f0564390a180e62fe5d34
Payam Dehkordy <payam.dehkordy@gmail.com>
2023-02-21 23:21:49 +0400

    Add API class to APIUtils

--- API/APIUtils/Client.cs
+++ API/APIUtils/Client.cs
@@ -13,25 +13,25 @@ namespace API.APIUtils
         private static string URL = "https://stackoverflow.com";
         private static string Username = "";
         private static string Password = "";
-        private Client _currentClient;
-        private RestClient _Client;
+        private static Client _currentClient;
+        private static RestClient _Client;
 
         private Client()
         {
             _Client = new RestClient(URL);
         }
 
-        public Client Instance => _currentClient ?? (_currentClient = new Client());
+        public static Client Instance => _currentClient ?? (_currentClient = new Client());
 
-        public RestClient GetClient => _Client;
+        public static RestClient GetClient => _Client;
 
-        public RestClient GetClientAuth()
+        public static RestClient GetClientAuth()
         {
             _Client.Authenticator = new HttpBasicAuthenticator(Username,Password);
             return _Client;
         }
 
-        public void QuitClient()
+        public static void QuitClient()
         {
             _currentClient = null;
             _Client = null;
