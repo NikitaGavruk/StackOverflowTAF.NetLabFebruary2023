59f8710eda88fc1d14b63a44669d148e7e16bba4
ErikGit <ericmovsessian71@gmail.com>
2023-02-24 10:47:49 +0400

    Couple of feractorings

--- API/APIClient/ApiClient.cs
+++ API/APIClient/ApiClient.cs
@@ -1,8 +1,6 @@
 ﻿using API.Exceptions;
-using Core.Utils;
 using Newtonsoft.Json;
 using RestSharp;
-using RestSharp.Authenticators;
 using System;
 using System.IO;
 
@@ -13,8 +11,6 @@ namespace API.APIClient {
         private RestClient client;
         public RestRequest request { get; private set; }
         private const string baseUrl = "https://api.stackexchange.com/";
-        private static XML_Reader TestDatareader = new XML_Reader(@"..\..\..\UI\Tests\TestData.xml");
-        private static XML_Reader Endpointsreader = new XML_Reader(@"..\..\TestData\Endpoints.xml");
 
         public ApiClient(string endpoint = "") {
             SetUrl(endpoint);
@@ -24,12 +20,6 @@ namespace API.APIClient {
             string url = Path.Combine(baseUrl, endpoint);
             client = new RestClient(url);
             return client;
-        }
-
-        public void SetAuthenticator() {
-            string email = TestDatareader.GetTextFromNode("//Email");
-            string password = TestDatareader.GetTextFromNode("//Password");
-            client.Authenticator = new HttpBasicAuthenticator(email, password);
         }
 
         private RestRequest AddingParameters(ParameterType type,params (string key, string value)[] header) {
@@ -96,10 +86,6 @@ namespace API.APIClient {
             if (response != null)
                 return JsonConvert.DeserializeObject<T>(response.Content);
             throw new NullReferenceException("The response is null");
-        }
-
-        public string SerializeToString<T>(T model) where T : class, new() {
-            return JsonConvert.SerializeObject(model);
         }
 
     }
