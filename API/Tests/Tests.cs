2c08df19d86b5032579ce5ab772f0d19b8cd5d81
Payam Dehkordy <payam.dehkordy@gmail.com>
2023-02-27 12:51:49 +0400

    Add Badge Test

--- API/Tests/Tests.cs
+++ API/Tests/Tests.cs
@@ -1,26 +1,32 @@
 ﻿using API.JsonModels;
+using API.Units;
 using Core.Utils;
 using NUnit.Framework;
 using RestSharp;
+using System;
 using System.Collections.Generic;
 
-namespace API.Tests {
+namespace API.Tests
+{
 
     [TestFixture]
-    internal class Tests:BaseTest {
+    internal class Tests : BaseTest
+    {
 
         private static XML_Reader reader = new XML_Reader(@"..\..\TestData\Endpoints.xml");
         API.APIUtils.API helper = new API.APIUtils.API();
 
-        private static IEnumerable<TestCaseData> ErrorModels() {
+        private static IEnumerable<TestCaseData> ErrorModels()
+        {
             yield return new TestCaseData(new ClientError { error_id = 403, error_message = "access_token was not created with write_access scope", error_name = "access_denied" },
                 "application/x-www-form-urlencoded", "key=jQbY46LJUbCnabOYlcGOBw((&access_token=Mc(9h6qsuI2HT3mI(TNTfA))&preview=true&filter=default&site=stackoverflow")
                 .SetName("Is Access Token ReadOnly");
         }
-        
+
         [Category("API Negative Tests")]
         [Test, TestCaseSource(nameof(ErrorModels))]
-        public void AnswerUpvote(ClientError errorModel, string key, string value) {
+        public void AnswerUpvote(ClientError errorModel, string key, string value)
+        {
 
             //Arrange
             string resourseEndpoint = string.Format(reader.GetTextFromNode("//Per-SiteMethods/answers/item/key"), reader.GetTextFromNode("//Per-SiteMethods/answers/item/value"));
@@ -35,8 +41,28 @@ namespace API.Tests {
             Assert.That(errorModel.error_id, Is.EqualTo(model.error_id));
             Assert.That(errorModel.error_message, Is.EqualTo(model.error_message));
             Assert.That(errorModel.error_name, Is.EqualTo(model.error_name));
-            
+
         }
+
+        [Test, Description("test the badges api")]
+        public void GetBadges()
+        {
+
+            //Arrange
+            string resourseEndpoint = "/2.3/badges";
+
+            //Act
+            RestRequest request = helper.CreateGetRequest(resourseEndpoint);
+            request.AddParameter("application/x-www-form-urlencoded", "?order=desc&sort=rank&site=stackoverflow",ParameterType.QueryString);
+            RestResponse response = helper.GetResponse(request);
+
+            //Assert
+            Root root = helper.DeserializeToClass<Root>(response);
+
+           // Console.WriteLine(root.items[0].badge_id);
+           // Assert.That(root.items[1].badge_id, Is.EqualTo(2068));
+        }
+
 
     }
 
