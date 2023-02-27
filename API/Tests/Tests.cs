72964167d5751bc0faf857602423b35d3f16215b
Davit Simonyan <davitsimonyan74@gamil.com>
2023-02-27 12:16:16 +0400

    Automated Api test case

--- API/Tests/Tests.cs
+++ API/Tests/Tests.cs
@@ -2,7 +2,9 @@
 using Core.Utils;
 using NUnit.Framework;
 using RestSharp;
+using System;
 using System.Collections.Generic;
+using System.Net;
 
 namespace API.Tests {
 
@@ -30,6 +32,20 @@ namespace API.Tests {
             Assert.That(errorModel.error_message, Is.EqualTo(model.error_message));
             Assert.That(errorModel.error_name, Is.EqualTo(model.error_name));
             
+        }
+
+        [Category ("API Negative Tests")]
+        [Test]
+        public void GetWithWrongURI()
+        {
+            string endPoint = reader.GetTextFromNode("//Per-SiteMethods/answers/wrongURI");
+            logger.Info("Send Get request with wrong URI");
+            RestRequest request = helper.CreateGetRequest(endPoint);
+            logger.Info("Get response");
+            RestResponse response = helper.GetResponse(request);
+            logger.Info("Verify that statuss code is 400(Bad Request)");
+            Assert.That(response.StatusCode == HttpStatusCode.BadRequest);
+
         }
 
         private static IEnumerable<TestCaseData> ErrorModels() {
