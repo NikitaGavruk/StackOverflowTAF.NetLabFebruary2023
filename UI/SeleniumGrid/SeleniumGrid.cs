6fb1319ffd42e348b0478230893937e3b0833046
Laura Hambardzumyan <90081975+LauraHambardzumyan@users.noreply.github.com>
2023-03-02 12:08:17 +0400

    selenium grid

---
+++ UI/SeleniumGrid/SeleniumGrid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumGridExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ChromeOptions();
+            options.AddAdditionalCapability(CapabilityType.BrowserName, "chrome");
+
+            var driver = new ChromeDriver(options);
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            var searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("Selenium Grid example");
+            searchBox.Submit();
+
+            driver.Quit();
+        }
+    }
+}
+
