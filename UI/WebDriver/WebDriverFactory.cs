e9f7ba8a73be68a3bf688660bd2e758ff9e2e4bd
Laura Hambardzumyan <90081975+LauraHambardzumyan@users.noreply.github.com>
2023-03-03 11:53:04 +0400

    WebDriverFactory

--- UI/WebDriver/WebDriverFactory.cs
+++ UI/WebDriver/WebDriverFactory.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using System;
 
 namespace AutomationTeamProject.WebDriver
@@ -8,15 +9,18 @@ namespace AutomationTeamProject.WebDriver
 
     public class WebDriverFactory
     {
-
         public enum BrowserType
         {
             Chrome,
-            Firefox
+            Firefox,
+            RemoteChrome,
+            RemteFireFox
         }
 
         public static IWebDriver GetDriver(BrowserType browser)
         {
+            Uri gridHubUrl = new Uri("http://localhost:4444/wd/hub");
+
             IWebDriver WebDriver = null;
             switch (browser)
             {
@@ -28,6 +32,22 @@ namespace AutomationTeamProject.WebDriver
                 case BrowserType.Firefox:
                     {
                         WebDriver = new FirefoxDriver();
+                        break;
+                    }
+                case BrowserType.RemoteChrome:
+                    {
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        RemoteWebDriver chromeDriver = new RemoteWebDriver(gridHubUrl, chromeOptions.ToCapabilities());
+                        chromeDriver.Navigate().GoToUrl("https://www.google.com");
+                        chromeDriver.Quit();
+                        break;
+                    }
+                case BrowserType.RemteFireFox:
+                    {
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                        RemoteWebDriver firefoxDriver = new RemoteWebDriver(gridHubUrl, firefoxOptions.ToCapabilities());
+                        firefoxDriver.Navigate().GoToUrl("https://www.google.com");
+                        firefoxDriver.Quit();
                         break;
                     }
             }
