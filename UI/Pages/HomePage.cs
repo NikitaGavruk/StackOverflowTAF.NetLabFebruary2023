6498b810f146024e3167a7140c7047e61ca4d5bc
ErikGit <ericmovsessian71@gmail.com>
2023-03-03 11:40:54 +0400

    commit

---
+++ UI/Pages/HomePage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using UI.Utils;
+
+namespace UI.Pages {
+    internal class HomePage:AbstractPage {
+        
+        private static readonly By overFlowBlogFirstItem = By.XPath("//div[contains(@Class,'ow-break-word')]");
+        private static readonly By companiesButton = By.XPath("//div[contains(text(),'Companies')]");
+
+        public bool IsPageLoaded() =>
+            WebDriverExtension.IsElementClickable(overFlowBlogFirstItem, 5);
+
+
+        public CompaniesPage ClickOnCompaniesButton() {
+            WebDriverExtension.MouseDownByJS(companiesButton, 5);
+            return new CompaniesPage();
+        }
+
+    }
+}
---
+++ UI/Tests/Tests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using UI.Entities;
+using UI.Pages;
+using UI.Steps;
+using UI.Utils;
+
+namespace UI.Tests {
+
+    [TestFixture]
+    internal class Tests:BaseTest {
+        
+        private static readonly XML_Reader xmlReader = new XML_Reader(@"..\..\..\Tests\TestData.xml");
+        private static readonly string email = xmlReader.GetTextFromNode("//Email");
+        private static readonly string password = xmlReader.GetTextFromNode("//Password");
+        private static readonly string tagToSearch = xmlReader.GetTextFromNode("//TagToSearch");
+        User user = new User(email, password);
+        HomePage homePage;
+        CompaniesPage companiesPage;
+        CompanyPageSteps companiesPageSteps;
+        GenericSearchedCompanyPage genericSearchedPage;
+
+        [Test]
+        public void Test() {
+
+            //Login 
+            homePage = new LoginPageSteps().Login(user);
+            //Assert that the home page has loaded correctly
+            Assert.That(homePage.IsPageLoaded());
+
+            //Go To The CompaniesPage
+            companiesPage = homePage.ClickOnCompaniesButton();
+            //Assert that the companies page has loaded correctly
+            Assert.That(companiesPage.IsPageLoaded());
+
+            //Click On The Filter By Tag Button
+            companiesPage.ClickonFilterByTagButton();
+            //Assert that The FilterPane Pane is Visible
+            Assert.That(companiesPage.IsFilterPaneVisible());
+
+            //Input "rust" in the searchbox of the filter by tag pane
+            companiesPage.InputInTheFilterByTagSearchBox();
+            //Assert that the suggestions appear after inputting
+            Assert.That(companiesPage.IsSuggestionsPaneVisisble);
+
+            //Click on the suggestion if it exactly matches the input tag
+            companiesPage.ClickOnTheElementThatCompletelyMatchesTheSearchCriteria();
+            //Assert thatthe suggestions pane disappeared and the clicked tag got fixed
+            //on the filter by tag searchbox
+            Assert.That(companiesPage.IsSuggestionsPaneDisappeared());
+            Assert.That(companiesPage.IsClickedTagFixedOnTheSearchBar());
+
+            //Click on the apply filter button
+            companiesPage.ClickApplyFilterButton();
+            companiesPageSteps = new CompanyPageSteps();
+            //Assert That the correct events happened after clicking apply filter 
+            Assert.That(companiesPageSteps.IsFilterAppliedCorrectly(out int countOfCompanies));
+            
+            //Create Instance of a class which included all the URLs for all the 
+            //pages returned By search
+            genericSearchedPage =
+            GenericSearchedCompanyPage.CreateInstance(companiesPage.GetUrls(countOfCompanies));
+            //Assert That The Searched Tag is included in each Company's techstack list
+            Assert.That(genericSearchedPage.IsTagExists(countOfCompanies, tagToSearch));
+
+        }
+
+    }
+
+}
