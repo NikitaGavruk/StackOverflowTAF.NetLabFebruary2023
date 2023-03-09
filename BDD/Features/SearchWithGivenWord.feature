1f628703eb5e390632c33e9db08c1ad3f4c1b240
Laura Hambardzumyan <90081975+LauraHambardzumyan@users.noreply.github.com>
2023-03-09 21:29:48 +0400

    Changes to BDD

---
+++ BDD/Features/SearchWithGivenWord.feature
@@ -0,0 +1,11 @@
+﻿   Feature:Search Testing
+    As a user
+    I want to be able to search for items on the website
+    So that I can find the information I need quickly and easily
+
+   @BDD_UI
+   Scenario: Search for an item with a given word
+		Given I should see the search bar
+		When I execute search with a given word
+		Then I should see search results
+
--- BDD/StepDefinitions/SearchTestingStepDefinitions.cs
+++ BDD/StepDefinitions/SearchTestingStepDefinitions.cs
@@ -1,11 +1,6 @@
-﻿using AutomationTeamProject.WebDriver;
+using AutomationTeamProject.WebDriver;
 using Core.Logger;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 using UI.Pages;
 using UI.Steps;
@@ -13,33 +8,33 @@ using UI.Steps;
 namespace BDD.StepDefinitions
 {
     [Binding]
-    public class SearchFunctionalityStepDefinitions
+    public class SearchTestingStepDefinitions
     {
         private ScenarioContext scenarioContext;
-        SearchResultPage searchResultPage = new SearchResultPage();
         SearchResultPageSteps searchResultPageSteps = new SearchResultPageSteps();
         GeneralPage generalPage = new GeneralPage();
-        public SearchFunctionalityStepDefinitions(ScenarioContext scenarioContext)
+
+        public SearchTestingStepDefinitions(ScenarioContext scenarioContext)
         {
             this.scenarioContext = scenarioContext;
         }
 
-        [Given(@"the search bar is visible")]
-        public void GivenTheSearchBarIsVisible()
+        [Given(@"I should see the search bar")]
+        public void GivenIShouldSeeTheSearchBar()
         {
             scenarioContext.Get<Logger>("logger").Info("Search bar should be visible");
             Assert.That(generalPage.IsSearchBarVisible());
         }
 
-        [When(@"executes the search request")]
-        public void WhenExecutesTheSearchRequest()
+        [When(@"I execute search with a given word")]
+        public void WhenIExecuteSearchWithAGivenWord()
         {
             scenarioContext.Get<Logger>("logger").Info("Execute search with a given word");
             generalPage.ExecuteSearchRequest(".gitignore");
         }
 
-        [Then(@"the search results should be displayed within (.*) seconds")]
-        public void ThenTheSearchResultsShouldBeDisplayedWithinSeconds(int p0)
+        [Then(@"I should see search results")]
+        public void ThenIShouldSeeSearchResults()
         {
             scenarioContext.Get<Logger>("logger").Info("Search is done correctly");
             Assert.That(searchResultPageSteps.IsSearchDoneCorrectly());
